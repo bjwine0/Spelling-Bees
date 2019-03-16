187feecede3b7c9fcd046cd3c3dfe460a3cdb470
bjwine0 <bradsmilek@gmail.com>
2019-03-16 09:07:38 -0700

    have level 1 image showing with function showing 10 random alphabet letters

--- js/app.js
+++ js/app.js
@@ -8,10 +8,21 @@ var levelTwoImg = ['bubble', 'apple', 'dragon'];
 console.log('leveTwoImg', levelTwoImg);
 var levelThreeImg = ['butterfly', 'elephant', 'strawberry'];
 console.log('levelThreeImg', levelThreeImg);
+
+
 var allAlphabets = [];
 console.log('allAlphabets', allAlphabets);
-var allImagePic = [];
-console.log('allImagePic', allImagePic);
+
+var levelOnePics = [];
+console.log('levelone', levelOnePics);
+
+var levelTwoPics = [];
+console.log('leveltwo', levelTwoPics);
+
+var levelThreePics = [];
+console.log('level3', levelThreePics);
+
+
 
 function ImageAlph(name, ext) {
   this.name = name;
@@ -50,27 +61,100 @@ new ImageAlph('Z', 'jpg');
 
 
 
-function ImagePic (name, ext) {
+function levelOne (name, ext) {
   this.name = name;
   this.ext = ext;
   this.filePath = `img/${name}.${ext}`;
   this.clicks = 0;
-  allImagePic.push(this);
+  levelOnePics.push(this);
 }
 
-new ImagePic('squiggle', 'jpg');
-new ImagePic('alph', 'png');
-new ImagePic('bee', 'jpg');
-new ImagePic('', '');
-new ImagePic('', '');
-new ImagePic('', '');
-new ImagePic('', '');
-new ImagePic('', '');
-new ImagePic('', '');
+new levelOne('squiggle', 'jpg');
+new levelOne('alph', 'png');
+new levelOne('bee', 'jpg');
 
 
+function levelTwo (name, ext) {
+  this.name = name;
+  this.ext = ext;
+  this.filePath = `img/${name}.${ext}`;
+  this.clicks = 0;
+  levelTwoPics.push(this);
+}
+
+new levelTwo('squiggle', 'jpg');
+new levelTwo('alph', 'png');
+new levelTwo('bee', 'jpg');
+
+function levelThree (name, ext) {
+  this.name = name;
+  this.ext = ext;
+  this.filePath = `img/${name}.${ext}`;
+  this.clicks = 0;
+  levelThreePics.push(this);
+}
+
+new levelThree('squiggle', 'jpg');
+new levelThree('alph', 'png');
+new levelThree('bee', 'jpg');
 
 
+function removeButton() {
+  var elem = document.getElementById('replay');
+  elem.parentNode.removeChild(elem);
+}
+removeButton();
 
+function recallButton() {
+  var element = document.getElementById('buttons');
+  var elem = document.createElement('button');
+  element.appendChild(elem);
+}
+//***********Recall button work */
+
+
+function startButtonBye () {
+  var elem = document.getElementById('start');
+  elem.parentNode.removeChild(elem);
+
+}
+
+document.getElementById('start').addEventListener('click', startFunction);
+
+function startFunction () {
+  getPicElem.src = levelOnePics[0].filePath;
+  startButtonBye();
+  placeTen();
+  // function calling 10 alphabets letters
+
+  
+}
+var tenRandom = []; //used for local storage later
+
+function placeTen() {
+  debugger;
+  var currentTen = [];
+  var imgEl = [];
+  for (var i = 0; i < 10; i++) {
+    
+    currentTen[i] = getRandom();
+    // var liElem = document.createElement('li');  // maybe use list element and append image to it?
+    var imgElem = document.createElement('img');
+    imgElem.src = allAlphabets[currentTen[i]].filePath;
+    // var imgSource = imgEl.src;
+    alphabetElem.appendChild(imgElem);
+    // imgElem.append(imgEl);
+    // imgElem[i].src = allAlphabets[currentTen[i]].filePath;
+  }
+  console.log('currentten', currentTen);
+  for (var j = 0; i < currentTen.length; j++) {
+    imgEl[j].src = allAlphabets[currentTen[j]].filePath;
+  }
+}
+
+function getRandom() {
+  return Math.floor(Math.random() * allAlphabets.length);
+  
+}
 
 
